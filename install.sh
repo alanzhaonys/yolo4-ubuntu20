73edee1a396cb1f45a9057a12593f6a5f5a0ac4e
Alan Zhao <azhao6060@gmail.com>
2020-09-22 00:12:06 -0400

    updates

--- install.sh
+++ install.sh
@@ -22,16 +22,13 @@ mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
 apt install ./cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
 apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
-apt-get -y install cuda
+apt install cuda
+rm cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
 
-#wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-#apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-#add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+echo "export PATH=/usr/local/cuda-11.0/bin${PATH:+:${PATH}}" >> ~/.bashrc
+source ~/.bashrc
 
 # Reboot
-
-#echo "export PATH=/usr/lib/cuda/bin${PATH:+:${PATH}}" >> ~/.bashrc
-source ~/.bashrc
 
 # opencv
 apt install libopencv-dev python3-opencv
@@ -43,6 +40,50 @@ opencv_version
 # 1. cuDNN Runtime library for Ubuntu 18.04 (Deb)
 # 2. cuDNN Developer library for Ubuntu 18.04 (Deb)
 # 3. cuDNN Code Samples and User Guide for Ubuntu 1804 (Deb)
-dpkg -i cuda/libcudnn8_8.0.2.39-1+cuda10.1_amd64.deb
-dpkg -i cuda/libcudnn8-dev_8.0.2.39-1+cuda10.1_amd64.deb
-dpkg -i libcudnn8-doc_8.0.2.39-1+cuda10.1_amd64.deb
+
+dpkg -i cuda/cuda11.0/libcudnn8_8.0.2.39-1+cuda11.0_amd64.deb
+dpkg -i cuda/cuda11.0/libcudnn8-dev_8.0.2.39-1+cuda11.0_amd64.deb 
+dpkg -i cuda/cuda11.0/libcudnn8-doc_8.0.2.39-1+cuda11.0_amd64.deb
+
+# Verify cuDNN
+# cp -r /usr/src/cudnn_samples_v8/ $HOME
+# cd  $HOME/cudnn_samples_v8/mnistCUDNN
+# make clean && make
+# ./mnistCUDNN
+
+# OpenMP (for CPU)
+apt install libomp-dev
+
+# YOLO v4
+git clone https://github.com/AlexeyAB/darknet
+cd darknet
+
+# Modify Makefile
+# For CPU
+GPU=0
+CUDNN=0
+CUDNN_HALF=0
+OPENCV=1
+AVX=1
+OPENMP=1
+LIBSO=1
+ZED_CAMERA=0
+ZED_CAMERA_v2_8=0
+
+# For GPU
+GPU=1
+CUDNN=1
+CUDNN_HALF=1
+OPENCV=1
+AVX=0
+OPENMP=0
+LIBSO=1
+ZED_CAMERA=0
+ZED_CAMERA_v2_8=0
+
+Run: make
+
+# Testing YOLO
+# Download yolov4.weights https://drive.google.com/file/d/1cewMfusmPjYWbrnuJRuKhPMwRe_b9PaT/view
+
+./darknet detect cfg/yolov4.cfg yolov4.weights data/person.jpg
